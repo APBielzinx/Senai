ba5768a63842995ac636f48afb77339fee9dc15c
APBielzinx <112442554+APBielzinx@users.noreply.github.com>
2023-04-29 12:31:18 -0300

    zap

--- controller/zap.php
+++ controller/zap.php
@@ -2,9 +2,24 @@
 
 $nome = $_POST['nome'];
 $telefone = $_POST['telefone'];
-$email = $_POST['text'];
+$email = $_POST['email'];
+$text = $_POST['text'];
 
-echo $nome;
+$textformat = str_replace(' ','%20',$text);
+
+echo("
+
+
+<script>
+
+window.location.href = 'https://wa.me/5511968210285?text=olá%20me%20chamo%20$nome%20meu%20email%20para%20contato%20é:%20$email%20meu%20problema%20é%20%20%20$textformat'
+
+</script>
+
+
+"
+);
+
 
 
 
